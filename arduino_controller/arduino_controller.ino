7aaff35d022b4af6070fa455ed9736fcf2d3a373
Alexandre FALTOT <extern.alexandre.faltot@europcar.com>
2018-03-16 20:31:26 +0100

    Made a GUI

--- arduino_controller/arduino_controller.ino
+++ arduino_controller/arduino_controller.ino
@@ -1,3 +1,4 @@
+#include <LiquidCrystal.h>
 #include <Keypad.h>
 
 const byte ROWS = 4; //four rows
@@ -13,7 +14,7 @@ byte rowPins[ROWS] = {9, 8, 7, 6};
 byte colPins[COLS] = {5, 4, 3, 2};
 
 int intKeys[ROWS][COLS] = {
-  {1, 2, 3, 10},
+  {1, 2, 3, 11},
   {4, 5, 6, 12},
   {7, 8, 9, 13},
   {15, 0, 16, 14}
@@ -28,13 +29,19 @@ int  reverbValue;
 char key;
 
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+LiquidCrystal lcd(32, 30, 28, 26, 24, 22);
 
 void setup() {
   Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.print(" PureData Micro");
+  lcd.setCursor(0, 1);
+  lcd.print("   Controller");
   pinMode(pitchInterrupterPin, INPUT);
   pinMode(reverbInterrupterPin, INPUT);
   pinMode(pitchLevelPin, INPUT);
   pinMode(reverbLevelPin, INPUT);
+  delay(5000);
 }
 
 void loop() {
@@ -44,22 +51,38 @@ void loop() {
     Serial.flush();
   }
 
+  lcd.setCursor(0, 0);
   if (digitalRead(pitchInterrupterPin) == HIGH) {
     pitchValue = analogRead(pitchLevelPin);
     pitchValue = map(pitchValue, 0, 1020, 251, 151);
     Serial.write(pitchValue);
     Serial.flush();
+    float pitch = ((float) pitchValue - 201) / 10;
+    if (pitch < 0) {
+      lcd.print("Pitch:      " + String(pitch, 2));
+    } else {
+      lcd.print("Pitch:      +" + String(pitch, 2));
+    }
   } else {
     Serial.write(201);
     Serial.flush();
+    lcd.print("Pitch:       OFF");
   }
 
+  lcd.setCursor(0, 1);
   if (digitalRead(reverbInterrupterPin) == HIGH) {
     reverbValue = analogRead(reverbLevelPin);
     reverbValue = map(reverbValue, 0, 1020, 150, 50);
     Serial.write(reverbValue);
     Serial.flush();
+    float reverb = ((float) reverbValue - 50) / 100;
+    if (reverb < 0) {
+      lcd.print("Reverb:    " + String(reverb, 2));
+    } else {
+      lcd.print("Reverb:     " + String(reverb, 2));
+    }
   } else {
+    lcd.print("Reverb:      OFF");
     Serial.write(50);
     Serial.flush();
   }
